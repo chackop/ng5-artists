73e276cb8fc95c200dd4992dc5dec14ec8450b68
Chacko <chackop@gmail.com>
2020-09-08 00:24:45 +0100

    ng model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      .list-group-item:first-child {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-top: 0;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   query: string;
   artists: object;
 
+  showArtist(item) {
+    this.query = item.name;
+    item.highlight = !item.highlight;
+  }
+
   constructor() {
-    this.query = 'Barot';
+    this.query = '';
     this.artists = [
       {
         name: 'Barot Bellingham',
